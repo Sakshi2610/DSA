69246d1a247a2f76255ab16b12df81b15f24fbf3
Sakshi Rane <sakshiramchandrarane@gmail.com>
2024-03-27 20:32:36 +0530

    learn concepts of circular linked list

---
+++ circular_LL.c
@@ -0,0 +1,48 @@
+// Circular Linked List creation and Traversal in one code
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void linkedListTraversal(struct Node *head)
+{
+    struct Node *ptr = head;
+    do{
+        printf("Elements %d\n", ptr->data);
+        ptr = ptr->next;
+    }while (ptr != head);
+}
+
+int main(){
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+
+    head = (struct Node *)malloc(sizeof(struct Node)); //dynamic memory creation in heap using malloc
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));    
+
+    head->data = 7;
+    head->next = second;
+
+    second->data = 3;
+    second->next = third;
+
+    third->data = 4;
+    third->next = fourth;
+
+    fourth->data = 1;
+    fourth->next = head;
+
+    linkedListTraversal(head);
+    return 0;
+}
+
+
