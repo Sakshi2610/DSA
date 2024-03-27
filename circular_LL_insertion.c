61dfa6f22855c38aadc97d7cc14e81d8ce8d7254
Sakshi Rane <sakshiramchandrarane@gmail.com>
2024-03-27 21:06:26 +0530

    learn concept of insertion in circular linked list

--- circular_LL_insertion.c
+++ circular_LL_insertion.c
@@ -1,4 +1,8 @@
-
+// Insertion in a Circular Linked List
+// 1. Insertion at begining
+// 2. Insertion in between(at index)
+// 3. Insertion at end
+// 4. Insertion after node
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -16,6 +20,59 @@ void linkedListTraversal(struct Node *head)
         printf("Elements %d\n", ptr->data);
         ptr = ptr->next;
     }while (ptr != head);
+}
+
+struct Node * insertAtFirst(struct Node *head, int data){
+    struct Node * ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->data = data;
+
+    struct Node *p = head->next;
+    while(p->next != head){
+        p = p->next;
+    }
+    p->next = ptr;
+    ptr->next = head;
+    head = ptr;
+    return head;
+}
+
+struct Node * insertAtIndex(struct Node *head, int data, int index){
+    struct Node * ptr = (struct Node *)malloc(sizeof(struct Node));
+    struct Node * p = head;
+    ptr->data = data;
+    int i=0;
+
+    while (i != index-1)
+    {
+        p = p->next;
+        i++;
+    }
+    
+    ptr->next = p->next;
+    p->next = ptr;
+    return head;
+}
+
+struct Node * insertAtLast(struct Node *head, int data){
+    struct Node * ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->data = data;
+
+    struct Node *p = head->next;
+    while(p->next != head){
+        p = p->next;
+    }
+    p->next = ptr;
+    ptr->next = head;
+    return head;
+}
+
+struct Node * insertAfterNode(struct Node *head, struct Node *prevNode, int data){
+    struct Node * ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->data = data;
+
+    ptr->next = prevNode->next;
+    prevNode->next = ptr;
+    return head;
 }
 
 int main(){
@@ -41,6 +98,13 @@ int main(){
     fourth->data = 1;
     fourth->next = head;
 
+    printf("Before the Insertion in Linked List \n");
+    linkedListTraversal(head);
+    printf("\n After the Insertion in Linked List \n");
+    // head = insertAtFirst(head, 9);
+    // head = insertAtIndex(head, 9, 3);
+    // head = insertAtLast(head, 9);
+    head = insertAfterNode(head, third, 40);
     linkedListTraversal(head);
     return 0;
 }
